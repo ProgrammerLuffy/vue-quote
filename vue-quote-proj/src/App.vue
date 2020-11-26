8cb9e922651bab9b8e9a0e030d4e4ca3341bbb68
programmerLuffy <programmingerror77@gmail.com>
2020-11-26 08:43:44 +0545

    addedd

---
+++ vue-quote-proj/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <appQuoteGrid :quotes="quotes"></appQuoteGrid>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './component/QuoteGrid.vue';
+    export default {
+        data(){
+            return{
+                maxQuote:10,
+                quotes:['Just a Quote'],
+            }
+        },
+            components:{
+                'appQuoteGrid':QuoteGrid
+            }
+    }
+</script>
+
+<style>
+</style>
---
+++ vue-quote-proj/src/component/QuoteGrid.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <appQuote>{{ quote }}</appQuote>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import Quote from './Quote.vue';
+
+    export default{ 
+        props:['quotes'],
+        components:{
+            appQuote:Quote
+        }
+    }
+</script>
+
+<style>
+
+</style>>
